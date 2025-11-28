f227116de371090b16973e23eab314b1360a1c07
Andr1anos <andrian.syrbu@gmail.com>
2025-11-28 18:16:09 +0200

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-# main.py -- simple placeholder so render web service won't crash on startup
-if __name__ == "__main__":
-    print("schwalbe_price_monitor: placeholder main. Cron jobs perform the checks.")
